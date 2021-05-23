46de41914d452366d9abbcefa3b9106f40955944
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2021-05-23 19:41:24 +0200

    PWA new site version

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import reportWebVitals from "./reportWebVitals";
+
+import ServiceWorkerWrapper from "./ServiceWorkerWrapper";
 
 ReactDOM.render(
 	<React.StrictMode>
+		<ServiceWorkerWrapper />
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
@@ -15,9 +18,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
