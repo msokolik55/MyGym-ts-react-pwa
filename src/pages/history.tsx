10b549b17e59b61d20d5b5540192ee85d698fd02
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2021-05-23 11:36:28 +0200

    Merged styles into variable

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { history } from "../data/interfaces";
 import { categories } from "../data/bodyParts";
+
 import { IconButton } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/styles";
 
 type props = {
 	data: history[];
 	setData: React.Dispatch<React.SetStateAction<history[]>>;
 };
 
+const useStyles = makeStyles({
+	flexRow: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center"
+	},
+	flexColumn: {
+		display: "flex",
+		flexDirection: "column"
+	}
+});
+
 const HistoryPage = ({ data, setData }: props) => {
+	const classes = useStyles();
+
 	return (
-		<div style={{ display: "flex", flexDirection: "column" }}>
+		<div className={classes.flexColumn}>
 			{data !== undefined &&
 				data.map((item, id) => (
-					<div style={{ display: "flex", flexDirection: "column" }}>
-						<div style={{ display: "flex", alignItems: "center" }}>
+					<div className={classes.flexColumn}>
+						<div className={classes.flexRow}>
 							<IconButton
 								onClick={() => setData(data.filter((it) => it !== item))}>
 								<Delete />
