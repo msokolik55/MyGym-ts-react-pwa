0e343f2ab66b5609d43b30f7ba134a05db6140f8
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2022-02-06 00:16:37 +0100

    Added deleting exercises from DB

--- src/pages/reset.tsx
+++ src/pages/reset.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { del } from "idb-keyval";
+import { dbKeys } from "../data/database";
+import { Button } from "@material-ui/core";
 
 const ResetPage = () => {
-	return <div></div>;
+	return (
+		<>
+			{/* TODO: popup confirm window; popup about completed */}
+			<Button variant="outlined" onClick={() => del(dbKeys.exercises)}>
+				Vymazať dáta cvičení
+			</Button>
+			{/* ENDTODO */}
+		</>
+	);
 };
 
 export default ResetPage;
