55a03032c7becebef31411365c484dee7b4aa284
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2022-02-08 15:45:24 +0100

    Added confirm Dialog
    - Dialog for confirm/abort deleting saved exercises

--- src/pages/reset.tsx
+++ src/pages/reset.tsx
@@ -1,16 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { del } from "idb-keyval";
 import { dbKeys } from "../data/database";
-import { Button } from "@material-ui/core";
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Snackbar
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 
 const ResetPage = () => {
+	const [openSnackbarSuccess, setOpenSnackbarSuccess] = useState<boolean>(false);
+	const [openSnackbarFail, setOpenSnackbarFail] = useState<boolean>(false);
+	const [openDialog, setOpenDialog] = useState<boolean>(false);
+
 	return (
 		<>
-			{/* TODO: popup confirm window; popup about completed */}
-			<Button variant="outlined" onClick={() => del(dbKeys.exercises)}>
+			<Button variant="outlined" onClick={() => setOpenDialog(true)}>
 				Vymazať dáta cvičení
 			</Button>
-			{/* ENDTODO */}
+
+			<Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+				<DialogTitle>Odstranenie cviceni</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						Naozaj si prajete zmazat vsetky ulozene cvicenia? Dany krok sa
+						neda vratit.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button
+						onClick={() => {
+							setOpenDialog(false);
+							setOpenSnackbarFail(true);
+						}}>
+						Nie
+					</Button>
+					<Button
+						onClick={() => {
+							del(dbKeys.exercises).then(() =>
+								setOpenSnackbarSuccess(true)
+							);
+							setOpenDialog(false);
+						}}
+						autoFocus>
+						Ano
+					</Button>
+				</DialogActions>
+			</Dialog>
+
+			<Snackbar
+				open={openSnackbarSuccess}
+				autoHideDuration={2000}
+				anchorOrigin={{
+					horizontal: "center",
+					vertical: "bottom"
+				}}
+				onClose={() => setOpenSnackbarSuccess(false)}>
+				<Alert severity="success">Mazanie uspesne</Alert>
+			</Snackbar>
+
+			<Snackbar
+				open={openSnackbarFail}
+				autoHideDuration={2000}
+				anchorOrigin={{
+					horizontal: "center",
+					vertical: "bottom"
+				}}
+				onClose={() => setOpenSnackbarFail(false)}>
+				<Alert severity="info">Mazanie zrusene</Alert>
+			</Snackbar>
 		</>
 	);
 };
