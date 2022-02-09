14af8cc11c4f5c29bf4461d59c8635c832671f05
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2022-02-09 03:09:03 +0100

    Added date range
    - moved help functions to HistoryPage
    - displaying exercises between range

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { bodyPart, history } from "../data/interfaces";
 import { categories } from "../data/bodyParts";
 
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+
 import {
 	Card,
 	Divider,
 	FormControl,
+	Grid,
 	IconButton,
 	InputLabel,
 	MenuItem,
@@ -13,6 +17,9 @@ import {
 } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/styles";
+import { DatePicker } from "@mui/lab";
+import TextField from "@mui/material/TextField";
+import skLocale from "date-fns/locale/sk";
 
 type props = {
 	data: history[];
@@ -40,32 +47,117 @@ const HistoryPage = ({ data, setData }: props) => {
 	const classes = useStyles();
 
 	const [selectedPart, setSelectedPart] = useState<number | undefined>(undefined);
-	const getCategoryCount = (cat: bodyPart) => {
-		return data.filter((item) => item.category === cat.key).length;
+	const getTrainingsCount = (cat: bodyPart | undefined) => {
+		return data
+			.filter((item) => cat === undefined || item.category === cat.key)
+			.filter((item) => betweenDates(item)).length;
+	};
+
+	const betweenDates = (item: history) => {
+		return (
+			(dateFrom === null || new Date(item.date) >= dateFrom) &&
+			(dateTo === null || new Date(item.date) <= dateTo)
+		);
+	};
+
+	const [dateFrom, setDateFrom] = useState<Date | null>(null);
+	const [dateTo, setDateTo] = useState<Date | null>(null);
+
+	const formatTwoDigits = (num: number) => {
+		const str = num.toString();
+		return str.length === 1 ? "0" + str : str;
+	};
+
+	const formatDate = (date: Date) => {
+		let result =
+			formatTwoDigits(date.getDate()) +
+			"." +
+			formatTwoDigits(date.getMonth() + 1) +
+			"." +
+			date.getFullYear().toString() +
+			" " +
+			formatTwoDigits(date.getHours()) +
+			":" +
+			formatTwoDigits(date.getMinutes());
+		return result;
+	};
+
+	const resetTime = (date: Date, begin: boolean) => {
+		date.setHours(begin ? 0 : 23);
+		date.setMinutes(begin ? 0 : 59);
+		date.setSeconds(begin ? 0 : 59);
+		date.setMilliseconds(begin ? 0 : 999);
+		return date;
 	};
 
 	return (
 		<>
-			<FormControl style={{ marginLeft: "2em", minWidth: "5em" }}>
-				<InputLabel>Oblasť</InputLabel>
-				<Select
-					value={selectedPart}
-					onChange={(event) => {
-						setSelectedPart(event.target.value as number);
-					}}>
-					<MenuItem key="all" value={undefined}>
-						<em>Vsetky</em>
-					</MenuItem>
-					{categories.map((cat) => (
-						<MenuItem
-							key={cat.key}
-							value={cat.key}
-							disabled={data !== undefined && getCategoryCount(cat) === 0}>
-							{cat.name} ({data !== undefined && getCategoryCount(cat)})
+			<Grid className={classes.flexRow}>
+				<FormControl style={{ marginLeft: "2em", minWidth: "5em" }}>
+					<InputLabel>Oblasť</InputLabel>
+					<Select
+						value={selectedPart}
+						onChange={(event) => {
+							setSelectedPart(event.target.value as number);
+						}}>
+						<MenuItem key="all" value={undefined}>
+							<em>Vsetky</em>
 						</MenuItem>
-					))}
-				</Select>
-			</FormControl>
+						{categories.map((cat) => (
+							<MenuItem
+								key={cat.key}
+								value={cat.key}
+								disabled={
+									data !== undefined && getTrainingsCount(cat) === 0
+								}>
+								{cat.name} ({data !== undefined && getTrainingsCount(cat)}
+								)
+							</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+
+				<LocalizationProvider dateAdapter={AdapterDateFns} locale={skLocale}>
+					<div style={{ maxWidth: "9em" }}>
+						<DatePicker
+							label="Od"
+							value={dateFrom}
+							onChange={(newValue) => {
+								if (newValue !== null)
+									newValue = resetTime(newValue, true);
+
+								if (
+									newValue === null ||
+									dateTo === null ||
+									(dateTo !== null && newValue <= dateTo)
+								)
+									setDateFrom(newValue);
+							}}
+							renderInput={(params) => <TextField {...params} />}
+						/>
+					</div>
+					{" - "}
+					<div style={{ maxWidth: "9em" }}>
+						<DatePicker
+							label="Do"
+							value={dateTo}
+							onChange={(newValue) => {
+								if (newValue !== null)
+									newValue = resetTime(newValue, false);
+
+								if (
+									newValue === null ||
+									dateFrom === null ||
+									(dateFrom !== null && newValue >= dateFrom)
+								)
+									setDateTo(newValue);
+							}}
+							renderInput={(params) => <TextField {...params} />}
+						/>
+					</div>
+				</LocalizationProvider>
+			</Grid>
+
 			<Divider />
 			<div className={classes.flexReverse}>
 				{data !== undefined &&
@@ -75,6 +167,7 @@ const HistoryPage = ({ data, setData }: props) => {
 								selectedPart === undefined ||
 								item.category === selectedPart
 						)
+						.filter((item) => betweenDates(item))
 						.map((item) => (
 							<Card className={classes.flexColumn}>
 								<div className={classes.flexRow}>
@@ -85,7 +178,8 @@ const HistoryPage = ({ data, setData }: props) => {
 										<Delete />
 									</IconButton>
 									<p>
-										{item.date} {categories[item.category].name}
+										{formatDate(new Date(item.date))}{" "}
+										{categories[item.category].name}
 										{item.exercises[0].fullProgram !== undefined && (
 											<>
 												{" "}
